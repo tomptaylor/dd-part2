da3a8770558a3af67575f8533c921d55a2b4b17f
Tom Taylor <tomptaylor@gmail.com>
2019-02-15 18:11:28 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <HelloWorld :options="fruitOptions" /> <HelloWorld :options="cars" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      selectedFruit: "Apple",
+      fruitOptions: {
+        Apple: "Apple",
+        Banana: "Banana",
+        Blueberry: "Blueberry",
+        Kiwi: "Kiwi",
+        Pear: "Pear",
+        Pineapple: "Pineapple",
+        Watermelon: "Watermelon"
+      },
+      selectedCar: "Mustang",
+      cars: {
+        Mustang: "Mustang",
+        BMW: "BMW",
+        Honda: "Honda"
+      }
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="dropdown">
+    <select v-model="selectedOption">
+      <option v-for="(option, name) in options" :value="option">{{
+        name
+      }}</option>
+    </select>
+  </div></template
+>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    options: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      selectedOption: null
+    };
+  },
+  mounted() {
+    this.selectedOption = this.value;
+  },
+
+  watch: {
+    value: function(newValue) {
+      this.selectedOption = newValue;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
